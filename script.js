d4ed6df324dec0c1bb7408d37ea57c6894e7aac2
Seynabou Diagne <diagne.seynabou008@gmail.com>
2023-06-05 19:29:51 +0000

    Mise à jour du code

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.categorie-btn');
-const sousCat = document.querySelector('.sous-categorie');
-
-toggleBtn.addEventListener('click', function() {
-  sousCat.classList.toggle('show');
-});
